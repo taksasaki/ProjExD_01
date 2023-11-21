e4a7b770a6730626d3b5c1af16094aeff3313185
佐々木貴大 <takasaki@sasakitakashidainoMacBook-Pro.local>
2023-11-21 15:41:04 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -10,13 +10,14 @@ def main():
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img,10,1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        #screen.blit(kk_img,[300,200]) こうかとんを画面に表示
+        screen.blit(kk_imgs[1],[300,200]) #こうかとんを画面に表示
         pg.display.update()
         tmr += 1        
         clock.tick(10)
