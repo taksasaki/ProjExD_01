0aa103b2e49615f9c75c39fd2d596b0f07739b0a
佐々木貴大 <takasaki@sasakitakashidainoMacBook-Pro.local>
2023-11-21 15:33:29 +0900

    練習2

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img = pg.transform.flip(kk_img,True,False)
+    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        #screen.blit(kk_img,[300,200]) こうかとんを画面に表示
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
